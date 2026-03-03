3c04d56506bb1fe3d0b581081989ba2e9792ab19
Furqan Ahmad <133624627+furqanahmadganaie@users.noreply.github.com>
2026-03-03 12:33:12 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+// top of server.js / index.js
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+// then import mongoose/connect...
+
+import express from 'express';
+import dotenv from 'dotenv'
+
+import path from "path";
+import authRoutes from '../routes/auth.route.js';
+import { connectDB } from '../lib/db.js';
+dotenv.config();
+const app=express();
+
+const PORT = process.env.PORT;
+// const __dirname = path.resolve();
+//CqCFjIbMLu0g9XdW
+app.use(express.json()); //middleware  to ectrach the daata 
+
+// const PORT =3000;
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT,()=> {
+     console.log(`server running on ${PORT}`)
+    connectDB();
+});
